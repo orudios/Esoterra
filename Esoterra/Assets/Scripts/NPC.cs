e5a98a0b041dccf6ea75d1ac36b904124f98c88c
Shona <lowdensh@gmail.com>
2020-10-29 16:57:01 +0000

    LFS test

---
+++ Esoterra/Assets/Scripts/NPC.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(
+    fileName = "NPC file",
+    menuName = "NPC Files Archive")]
+public class NPC : ScriptableObject
+{
+    public string npcName;
+    [TextArea(3,15)]
+    public string[] npcDialogue;
+    [TextArea(3,15)]
+    public string[] playerDialogue;
+}
