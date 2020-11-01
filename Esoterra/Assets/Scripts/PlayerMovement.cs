8b85847cd196571838bb5fe57e6e07bd993f30b9
Vlad <vladgavinas@gmail.com>
2020-11-01 22:06:18 +0000

    Add basic first person movement, implement upper deck level in Bridge

---
+++ Esoterra/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float playerSpeed = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = transform.right * horizontalAxis + transform.forward * verticalAxis;
+
+        controller.Move(playerMovement * playerSpeed * Time.deltaTime);
+    }
+}
