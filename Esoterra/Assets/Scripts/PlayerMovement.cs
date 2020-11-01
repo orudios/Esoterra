12deb346e50700fc3cf09a94f59cca3820da00fa
Vlad <vladgavinas@gmail.com>
2020-11-01 23:15:51 +0000

    Tidy up hierarchy and add prefabs into categories

--- Esoterra/Assets/Scripts/PlayerMovement.cs
+++ Esoterra/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+
     public float playerSpeed = 12f;
-    // Start is called before the first frame update
+    public float gravityConstant = -9.81f;
+    public float playerJumpHeight = 3f;
+
+    public Transform groundChecker;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 playerVelocity;
+    bool grounded;
     void Start()
     {
         
@@ -15,11 +24,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundMask);
+
+        if (grounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
         Vector3 playerMovement = transform.right * horizontalAxis + transform.forward * verticalAxis;
 
         controller.Move(playerMovement * playerSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(playerJumpHeight * -2f * gravityConstant);
+        }
+
+        playerVelocity.y += gravityConstant * Time.deltaTime;
+
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
