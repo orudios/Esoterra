fc07857849b19f685d7c313ef9d5474eebc207bd
Shona <lowdensh@gmail.com>
2020-11-02 00:01:06 +0000

    basic structure for objectives

---
+++ Esoterra/Assets/Scripts/Combat System/Enemies/Alien.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Alien : MonoBehaviour, IEnemy
+{
+    public int ID {get; set;}
+    public float currentHealth;
+    public float maxHealth;
+    public int strengthLevel;
+    public int defenceLevel;
+
+    // Set this to be the same as the layer which the player has
+    public LayerMask aggressionLayer;
+
+
+    void Start()
+    {
+        ID = 0;
+        currentHealth = maxHealth;
+    }
+
+    public void DealDamage(int amount)
+    {
+        return;
+    }
+
+    public void ReceiveDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log(gameObject.name + " died, destroying");
+        Destroy(gameObject);
+    }
+}
