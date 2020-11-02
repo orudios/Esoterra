30aef28ac818f6fabdf979d0f0c1a421b1f2cb8f
Vlad <vladgavinas@gmail.com>
2020-11-02 02:56:35 +0000

    Add flickering lights and door open/close animation

---
+++ Esoterra/Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator doorAnimation;
+    private bool doorOpen = false;
+    
+    private void Awake()
+    {
+        doorAnimation = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            // doorAnimation.Play("glass_door_open", 0, 0.0f);
+            doorAnimation.SetBool("door_open", true);
+            doorOpen = true;
+        }
+        else
+        {
+            // doorAnimation.Play("glass_door_close", 0, 0.0f);
+            doorAnimation.SetBool("door_open", false);
+            doorOpen = false;
+        }
+    }
+}
---
+++ Esoterra/Assets/Scripts/DoorRaycast.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorRaycast : MonoBehaviour
+{
+    [SerializeField] private int rayLength = 5;
+    [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private string excludeLayerName = null;
+    [SerializeField] private KeyCode openDoorKey = KeyCode.Mouse0;
+    [SerializeField] private Image crosshair = null;
+
+    private DoorController rayCastObj;
+    private bool crosshairActive;
+    private bool doOnce;
+    private const string interactableTag = "InteractiveObject";
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        int mask = 1 << LayerMask.NameToLayer(excludeLayerName) | layerMaskInteract.value;
+
+        if (Physics.Raycast(transform.position, forward, out hit, rayLength, mask))
+        {
+            if (hit.collider.CompareTag(interactableTag))
+            {
+                if (!doOnce)
+                {
+                    rayCastObj = hit.collider.gameObject.GetComponent<DoorController>();
+                    AdjustCrosshair(true);
+                }
+
+                crosshairActive = true;
+                doOnce = true;
+
+                if (Input.GetKeyDown(openDoorKey))
+                {
+                    rayCastObj = GameObject.Find("medbay_door").GetComponent<DoorController>();
+                    rayCastObj.PlayAnimation();
+                }
+            }
+        }
+
+        else
+        {
+            if (crosshairActive)
+            {
+                AdjustCrosshair(false);
+                doOnce = false;
+            }
+        }
+    }
+
+    void AdjustCrosshair(bool on)
+    {
+        if (on && !doOnce)
+        {
+            crosshair.color = Color.red;
+        }
+        else
+        {
+            crosshair.color = Color.white;
+            crosshairActive = false;
+        }
+    }
+}
