7c555a24332c731d7d6046e88f480623bd77404f
Shona <lowdensh@gmail.com>
2020-11-28 02:36:48 +0000

    hierarchy organisation and interaction revamp

---
+++ Esoterra/Assets/Scripts/Interface/DebugScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DebugScreen : MonoBehaviour
+{
+    public GameObject debugCanvasText;
+
+    Text position;
+    Text lookingAt;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        position = GameObject.Find("Debug Canvas/Text/Position").GetComponent<Text>();
+        lookingAt = GameObject.Find("Debug Canvas/Text/Looking at").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        ToggleText();
+        UpdateText();
+    }
+
+    void ToggleText()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            if (debugCanvasText.gameObject.activeSelf) {
+                debugCanvasText.gameObject.SetActive(false);
+            } else {
+                debugCanvasText.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void UpdateText()
+    {
+        position.text = "Position: " + player.transform.position;
+        lookingAt.text = "Looking at: ";
+    }
+}
