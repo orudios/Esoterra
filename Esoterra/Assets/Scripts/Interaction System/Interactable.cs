3e828f865af59af6595b67214b19b7e20cf9fbbb
Shona <lowdensh@gmail.com>
2020-11-28 02:36:48 +0000

    hierarchy organisation and interaction revamp

--- Esoterra/Assets/Scripts/Interaction System/Interactable.cs
+++ Esoterra/Assets/Scripts/Interaction System/Interactable.cs
@@ -1,20 +1,109 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
-[CreateAssetMenu
-(
-    fileName = "New Interactable",
-    menuName = "Interactable (Scriptable Object)"
-)]
-public class Interactable : ScriptableObject
+public class Interactable : MonoBehaviour
 {
-    [Tooltip("Displayed above the Interactable in the World Space, and in the dialogue window.")]
+    [Header("Interactable Details")]
+    [Tooltip("A human-readable name displayed to the player.")]
     public string displayName;
+    [Tooltip("What the player will do with this Interactable.")]
     public string interactionVerb;
+    [Tooltip("How close the player needs to be to be able to interact.")]
+    [Range(2.0f, 8.0f)]
+    public float interactionDistance = 5f;
 
-    [Header("Dialogue Strings")]
-    [TextArea(3,15)] public string[] interactableDialogue;
-    [TextArea(3,15)] public string[] playerDialogue;
+    [Header("World Space Text")]
+    public TMP_Text displayNameText;
+    public TMP_Text interactionVerbText;
+
+    // Controls
+    string controlInteract = "e";
+
+    // Player
+    GameObject player;
+    float playerDistance;
+    
+    // Crosshair
+    Image crosshair;
+    RaycastHit hit;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        crosshair = GameObject.Find("Crosshair").GetComponent<Image>();
+
+        SetWorldSpaceText();
+    }
+
+    void SetWorldSpaceText()
+    {
+        displayNameText.GetComponent<TextMeshPro>().text = displayName;
+        interactionVerbText.GetComponent<TextMeshPro>().text =
+            interactionVerb + " [" + controlInteract.ToUpper() + "]";
+    }
+
+    void Update()
+    {
+        TryInteract();
+    }
+
+    void TryInteract()
+    {
+        if (PlayerInRange()) {
+
+            // Show the world space text for this interactable
+            SetActiveObjects(true, new GameObject[] {
+                displayNameText.gameObject,
+                interactionVerbText.gameObject
+            });
+
+            // If the player is looking at an interactable
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit)) {
+                if (hit.transform.gameObject.tag == "Interactable") {
+                    crosshair.color = Color.red;
+                    Debug.Log(hit.transform.name);
+                }
+                else {
+                    crosshair.color = Color.white;
+                }
+            }
+
+        }
+        // Player not in range
+        else {
+            SetActiveObjects(false, new GameObject[] {
+                displayNameText.gameObject,
+                interactionVerbText.gameObject
+            });
+        }
+    }
+
+    // Check if player is close enough to interact
+    bool PlayerInRange()
+    {
+        playerDistance = Vector3.Distance(this.transform.position, player.transform.position);
+        if (playerDistance <= interactionDistance) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Set the active state of multiple GameObjects at once
+    void SetActiveObjects(bool boolean, GameObject[] objects)
+    {
+        foreach (GameObject obj in objects){
+            obj.SetActive(boolean);
+        }
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interact() in Interactable class");
+    }
 }
---
+++ Esoterra/Assets/Scripts/Interaction System/NPC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPC : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Interact() in NPC class");
+    }
+}
