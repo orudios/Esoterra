f2a77e94e73330d932d90982878977c2cb8290df
amin213 <69519798+amin213@users.noreply.github.com>
2020-12-01 22:08:56 +0000

    player can die

---
+++ Esoterra/Assets/Scripts/Player/playerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerHealth : MonoBehaviour
+{
+    public float health;
+    // Start is called before the first frame update
+
+    public void receiveDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0f)
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        Debug.Log("Player has died");
+    }
+}
