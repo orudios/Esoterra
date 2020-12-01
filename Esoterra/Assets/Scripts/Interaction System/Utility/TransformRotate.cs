9507e1284f21f98051f30a8ece8600d51657177c
Shona <lowdensh@gmail.com>
2020-12-01 15:51:27 +0000

    add interaction changes to Olympus scene
    minor changes to prefabs
    cleaning of assets
    addition of some resources into the scene
    replacement of doors in the scene

---
+++ Esoterra/Assets/Scripts/Interaction System/Utility/TransformRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TransformRotate : MonoBehaviour
+{
+    [Header("Rotation Details")]
+    [Tooltip("How fast to rotate on the x axis.")]
+    [Range(0f, 180f)]
+    public float speedXAxis = 0f;
+    [Tooltip("How fast to rotate on the y axis.")]
+    [Range(0f, 180f)]
+    public float speedYAxis = 0f;
+    [Tooltip("How fast to rotate on the z axis.")]
+    [Range(0f, 180f)]
+    public float speedZAxis = 20f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(speedXAxis, speedYAxis, speedZAxis) * Time.deltaTime);
+    }
+}
