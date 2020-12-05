40e49288591d06f20ca414cdc8486317876a0520
Shona <lowdensh@gmail.com>
2020-12-05 00:37:12 +0000

    add repairable objects

---
+++ Esoterra/Assets/Scripts/Interaction System/Interactables/Resource.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Resource : Interactable
+{
+    [Header("Resource Details")]
+    public int ID;
+    public int atomicNumber;
+    public string symbol;
+
+    [HideInInspector] public Inventory inventory;
+
+
+    void Reset()
+    {
+        interactionVerb = "Collect";
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Inventory>();
+    }
+
+    public override void Interact()
+    {
+        inventory.AddResource(ID, 1);
+        Destroy(transform.parent.gameObject);
+    }
+}
