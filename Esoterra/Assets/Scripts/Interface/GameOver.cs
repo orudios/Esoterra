aad37e8c9db445a6c91e6d6de6cea2154ddd0cb1
amin213 <69519798+amin213@users.noreply.github.com>
2020-12-06 22:01:08 +0000

    just incase of internet

---
+++ Esoterra/Assets/Scripts/Interface/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public bool gameOver = false;
+    public playerHealth health;
+
+    public GameObject gameOverUI;
+
+    void Start(){
+        gameOverUI.SetActive(false);
+        //hide the menu
+    }
+    void Update()
+    {
+        if (health.health<=0){
+            //if the player is dead
+            Debug.Log("Stopping game");
+            stopGame();
+        }
+    }
+
+    void stopGame(){
+        gameOverUI.SetActive(true);
+        //show the ending screen
+
+        //Cursor.lockState = CursorLockMode.None;
+        
+        Time.timeScale = 0f;
+        //freeze game
+        gameOver = true;
+
+    }
+
+    public void LoadMenu(){
+        Debug.Log("Load menu");
+    }
+}
