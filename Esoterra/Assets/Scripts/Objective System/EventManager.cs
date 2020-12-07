6e70a8d86794283807ed5feabd3a74dea4d51051
Vlad <vladgavinas@gmail.com>
2020-12-07 02:48:38 +0000

    Attempt to fix something

---
+++ Esoterra/Assets/Scripts/Objective System/EventManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventManager : MonoBehaviour
+{
+    // ResourceGoal
+    public delegate void ResourceEventHandler(int resID);
+    public static event ResourceEventHandler OnResourceCollect;
+
+    // RepairGoal
+    public delegate void RepairEventHandler(string repairableType);
+    public static event RepairEventHandler OnRepair;
+
+
+    public static void ResourceCollected(int resID)
+    {
+        if (OnResourceCollect != null) {
+            OnResourceCollect(resID);
+        }
+    }
+    
+    public static void Repaired(string repairableType)
+    {
+        Debug.Log("EventManager.Repaired(" + repairableType + ")");
+        if (OnRepair != null) {
+            OnRepair(repairableType);
+        }
+    }
+}
